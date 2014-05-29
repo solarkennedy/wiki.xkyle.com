623af90940a72f0dddc212e2904cd9c8fbd696ff
Kyle Anderson <kyle@xkyle.com>
2014-05-28 21:15:54 -0700

    Fix bigrename suffix

--- bigrename.sh
+++ bigrename.sh
@@ -5,7 +5,7 @@ do
   if [[ -f $EACH ]]; then
   OLD_NAME=`echo $EACH | sed 's/\.html//g'`
   echo "Finding files with references of $OLD_NAME and making them into $EACH"
-  find -type f | xargs sed "s/href=\"\/$OLD_NAME\"/href=\"\/$EACH.html\"/g" 
+  find -type f  | grep html | xargs sed -i "s/href=\"\/$OLD_NAME\"/href=\"\/$EACH\"/g" 
    
   fi
 done
