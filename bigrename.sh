707eeb72a56b9dfd276e07bbeabc0a5fa0013c27
Kyle Anderson <kyle@xkyle.com>
2014-05-29 07:19:47 -0700

    Convert hrefs to link to html

--- bigrename.sh
+++ bigrename.sh
@@ -5,7 +5,7 @@ do
   if [[ -f $EACH ]]; then
   OLD_NAME=`echo $EACH | sed 's/\.html//g'`
   echo "Finding files with references of $OLD_NAME and making them into $EACH"
-  find -type f  | grep html | xargs sed -i "s/href=\"\/$OLD_NAME\"/href=\"\/$EACH\"/g" 
+  sed -i "s/href=\"\/$OLD_NAME\"/href=\"\/$EACH\"/g" *.html
    
   fi
 done
