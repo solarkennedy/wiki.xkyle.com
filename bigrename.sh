0578387e21e393e189a2f4a421b899b4e1a846b8
Kyle Anderson <kyle@xkyle.com>
2014-05-28 21:13:38 -0700

    More bigrename fix

--- bigrename.sh
+++ bigrename.sh
@@ -4,7 +4,9 @@ for EACH in *
 do
   if [[ -f $EACH ]]; then
   OLD_NAME=`echo $EACH | sed 's/\.html//g'`
-  find -type f | xargs sed -i "s/$OLD_NAME/$EACH/g" *
+  echo "Finding files with references of $OLD_NAME and making them into $EACH"
+  find -type f | xargs sed "s/href=\"\/$OLD_NAME\"/href=\"\/$EACH.html\"/g" 
+   
   fi
 done
 
