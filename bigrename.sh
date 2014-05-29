da8e39976ebfbd30013838bc6d60150830e6a982
Kyle Anderson <kyle@xkyle.com>
2014-05-28 20:28:38 -0700

    Fix bigrename xargs

--- bigrename.sh
+++ bigrename.sh
@@ -4,7 +4,7 @@ for EACH in *
 do
   if [[ -f $EACH ]]; then
   OLD_NAME=`echo $EACH | sed 's/\.html//g'`
-  sed "s/$OLD_NAME/$EACH/g" *
+  find -type f | xargs sed -i "s/$OLD_NAME/$EACH/g" *
   fi
 done
 
